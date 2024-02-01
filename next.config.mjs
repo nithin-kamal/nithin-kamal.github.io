fd6db3ef4f65d349879a2ca312607aa2e3de2f05
Nithin <nithin777k@gmail.com>
2024-02-01 17:59:46 +0100

    next config mod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  unoptimized: true,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Nithin Kamal</h1>;
+  return (
+    <>
+      <h1>Nithin Kamal</h1>
+      <h2> ggg</h2>
+    </>
+  );
 }
