ac4eec06a561bd9e4098c56ec7dbc5fdc76ec884
Nithin <nithin777k@gmail.com>
2024-02-28 21:45:58 +0100

    scroll snap temp fix

--- src/components/main/Section/Section.tsx
+++ src/components/main/Section/Section.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties, ReactNode } from "react";
 
 const sectionDivStyle =
-  " min-h-screen w-full place-items-center place-content-center pt-[65px] flex text-center p-6  snap-start";
+  " min-h-screen w-full place-items-center place-content-center pt-[65px] flex text-center p-6  xs:snap-start";
 
 type SectionProps = {
   children: ReactNode;
@@ -14,6 +14,7 @@ const Section = ({ children, id, style, className }: SectionProps) => {
   return (
     <section id={id} className={className + sectionDivStyle} style={style}>
       {children}
+      <div className={"h-[40px] sm:h-[80px]"}></div>
     </section>
   );
 };
